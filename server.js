28d20d6d22dde2cf1a75dcaceac2388a5c426f45
Ty <nodedev21@protonmail.com>
2021-09-15 18:45:42 -0400

    add read for db

--- server.js
+++ server.js
@@ -9,25 +9,57 @@ const app= express();
 
 app.use(express.json());
 
-app.listen(port, ()=>{
-	console.log(`server is listening on port:${port}`)
-})
+app.listen(port, () => {
+	console.log(`server is listening on port:${port}`);
+});
 
 // CREATE
-app.post('/users',(req,res)=>{
-  // User.create()
-})
+app.post('/users', (req,res) => {
+  User.create(
+    {
+      name: req.body.newData.name,
+      email: req.body.newData.email,
+      password: req.body.newData.password
+    },
+    (err, data) => {
+      if (err) {
+        res.json({ success: false, message: err });
+      } else if (!data) {
+        res.json({ success: false, message: 'Not Found' });
+      } else {
+        res.json({ success: true, data: data });
+      }
+    }
+  );
+});
 
 app.route('/users/:id')
 // READ
-.get((req,res)=>{
-  // User.findById()
+  .get((req,res) => {
+    User.findById(req.params.id, (err, data) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: err
+        });
+      } else if (!data) {
+        res.json({
+          success: false,
+          message: 'Not Found'
+        });
+      } else {
+        res.json({
+          success: true,
+          data: data
+        });
+      }
+    });
 })
 // UPDATE
-.put((req,res)=>{
+.put((req,res) => {
   // User.findByIdAndUpdate()
 })
 // DELETE
-.delete((req,res)=>{
+.delete((req,res) => {
   // User.findByIdAndDelete()
-})
+});
